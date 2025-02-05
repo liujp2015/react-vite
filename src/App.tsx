222f4829900f0ccb7808a3baa0438d33288795ac
liujunpeng <392303104@qq.com>
2025-02-05 10:59:40 +0800

    add tailwind and add  tailwind css of MobileNavigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,30 @@
+import { useEffect } from "react";
 import Navigation from "./components/navigation";
 
 function App() {
+  useEffect(() => {
+    const MAX_FONT_SIZE = 40;
+    const html = document.querySelector("html");
+    // 根据窗口宽度计算 fontSize
+    const setFontSize = () => {
+      let fontSize = window.innerWidth / 10;
+      fontSize = fontSize > MAX_FONT_SIZE ? MAX_FONT_SIZE : fontSize;
+      if (html) {
+        html.style.fontSize = fontSize + "px";
+      }
+    };
+
+    // 监听页面加载完成后的字体大小设置
+    setFontSize();
+
+    // 监听窗口大小变化
+    window.addEventListener("resize", setFontSize);
+
+    // 清理副作用
+    return () => {
+      window.removeEventListener("resize", setFontSize);
+    };
+  }, []); // 空依赖数组，意味着只会在组件挂载时运行一次
   return (
     <>
       <Navigation></Navigation>
--- src/components/navigation/mobile/index.tsx
+++ src/components/navigation/mobile/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import { Category } from "../../../api/category";
 
 interface ChildComponentProps {
@@ -5,14 +6,30 @@ interface ChildComponentProps {
 }
 
 const MobileNavigation: React.FC<ChildComponentProps> = ({ categories }) => {
+  const liRef = useRef<HTMLLIElement>(null);
+
+  // 使用 useEffect 确保 DOM 元素已经挂载后再进行样式设置
+  useEffect(() => {
+    if (liRef.current) {
+      liRef.current.style.width = "60px";
+      liRef.current.style.transform = "translateX(0px)";
+    }
+  }, []); // 空依赖数组表示只在组件首次挂载时执行
+
   return (
     <>
-      <div className=""></div>
-      <div>
-        {(categories || []).map((item, index) => (
-          <div key={index}>{item.name}</div> // 假设 Category 包含 name 属性
-        ))}
-      </div>
+      <div className=" bg-white sticky top-0 left-0 z-10"></div>
+      <ul className=" relative flex overflow-x-auto p-1 text-xs text-zinc-600 overflow-hidden">
+        <li
+          className=" absolute bg-zinc-900 h-[22px] rounded-lg duration-200"
+          ref={liRef}
+        ></li>
+        <li className=" shrink-0 px-1.5 py-0.5 z-10 duration-200">
+          {(categories || []).map((item, index) => (
+            <div key={index}>{item.name}</div> // 假设 Category 包含 name 属性
+          ))}
+        </li>
+      </ul>
     </>
   );
 };
